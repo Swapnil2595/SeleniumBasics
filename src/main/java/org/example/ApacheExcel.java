ff2fa848aa6ea850a348ba20cf2f7d236ccd26f8
Swapnil2595 <swapnilrajpure892@gmail.com>
2025-02-14 11:55:44 +0530

    second commit

--- src/main/java/org/example/ApacheExcel.java
+++ src/main/java/org/example/ApacheExcel.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
@@ -16,16 +18,35 @@ public class ApacheExcel extends Browser{
 
     public static void main(String[] args) throws Exception {
 
-        File f = new File("C:\\Users\\Rushikesh\\IdeaProjects\\Selenium\\src\\test\\java\\New Microsoft Excel Worksheet.xlsx");
+        File f = new File("C:\\Users\\User\\OneDrive\\Desktop\\TestData.xlsx");
         FileInputStream file = new FileInputStream(f);
         XSSFWorkbook xs = new XSSFWorkbook(file);
         XSSFSheet sheet = xs.getSheetAt(0);
-        String username = sheet.getRow(1).getCell(2).getStringCellValue();
-        String password = sheet.getRow(1).getCell(3).getStringCellValue();
-        System.out.println(username);
-       // xs.close();
-        Browser.OpenBrowser("https://www.instagram.com/");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+           int rowcount= sheet.getLastRowNum();
+        int colcount = sheet.getRow(1).getLastCellNum();
+
+        for(int i = 0;i<=rowcount;i++){
+
+            XSSFRow row  = sheet.getRow(i);
+
+            for(int j=0;j<colcount;j++){
+
+                XSSFCell cell =row.getCell(j);
+
+                switch (cell.getCellType()){
+                    case STRING -> System.out.print(cell.getStringCellValue());
+                    case NUMERIC -> System.out.print(cell.getNumericCellValue());
+                    case BOOLEAN -> System.out.print(cell.getBooleanCellValue());
+                }
+                System.out.print(" | ");
+            }
+            System.out.println();
+        }
+
+//        Browser.OpenBrowser("https://www.instagram.com/");
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 //        SeleniumBasic.SendKeys(driver, By.xpath("//*[@id='name']"),username);
 //
 //        SeleniumBasic.ClickElement(driver,"//*[@id='email']");
@@ -33,18 +54,18 @@ public class ApacheExcel extends Browser{
 //        System.out.println(ot);
 //        xs.close();
 //        Browser.CloseBrowser();
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@name='username']")));
-        SeleniumBasic.ClickElement(driver,"//*[@name='username']");
-        SeleniumBasic.SendKeys(driver, By.xpath("//*[@name='username']"),username);
-        SeleniumBasic.ClickElement(driver,"//*[@name='password']");
-        SeleniumBasic.SendKeys(driver, By.xpath("//*[@name='password']"),password);
-        Thread.sleep(3000);
-        SeleniumBasic.ClickElement(driver,"//*[text()='Log in']");
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[text()='Not now']")));
-        SeleniumBasic.ClickElement(driver,"//*[text()='Not now']");
+//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@name='username']")));
+//        SeleniumBasic.ClickElement(driver,"//*[@name='username']");
+//        SeleniumBasic.SendKeys(driver, By.xpath("//*[@name='username']"),username);
+//        SeleniumBasic.ClickElement(driver,"//*[@name='password']");
+//        SeleniumBasic.SendKeys(driver, By.xpath("//*[@name='password']"),password);
+//        Thread.sleep(3000);
+//        SeleniumBasic.ClickElement(driver,"//*[text()='Log in']");
+//
+//        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[text()='Not now']")));
+//        SeleniumBasic.ClickElement(driver,"//*[text()='Not now']");
         xs.close();
-       Browser.CloseBrowser();
+   //    Browser.CloseBrowser();
     }
 
 }
